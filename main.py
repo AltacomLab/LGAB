9620609643582ec864f6fffb8f6305c32e750d06
khanhnthepu <khanhnth@epu.edu.vn>
2026-03-30 16:14:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 from src.dataset.load_datasets import upload_and_clean_csv
 
 def main():
-    print("🔹 Upload and clean dataset")
+    print(" Upload and clean dataset")
     df = upload_and_clean_csv()
-    print("✅ Dataset ready!")
+    print("Dataset ready!")
     
-    # TODO: Tiếp tục xử lý dữ liệu, train model, evaluate
-    # from src.models.gnn_model import GNNModel
-    # from src.training.trainer import train_model
-    # from src.evaluation.evaluator import evaluate_model
-
+ 
 if __name__ == "__main__":
     main()
